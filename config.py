1158df49348dffd5653c4b326e9bbebd7ab59020
Okoth Pius <orukopius8@gmail.com>
2022-02-03 14:13:23 +0300

    [Feat] Update

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+# When deploying to Heroku
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
