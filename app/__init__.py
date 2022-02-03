82b13ed359fb4a75d85c46292264da0bb39ba219
Okoth Pius <orukopius8@gmail.com>
2022-02-03 10:13:40 +0300

    [Refactor] Update app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
-from .config import DevConfig
+from flask_sqlalchemy import SQLAlchemy
 
-# Initializing application
-app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
 
+
+db = SQLAlchemy()
+
+app = Flask(__name__, instance_relative_config=True)
+
+# Load views
 from app import views
+
+# Setting up app configuration
+app.config.from_object('config')
+
+# Initializing flask extensions
+db.init_app(app)
