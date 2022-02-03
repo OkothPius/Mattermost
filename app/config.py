5a9c8482fc0242b22e3035843db0871fde9d2e6c
Okoth Pius <orukopius8@gmail.com>
2022-02-03 07:38:30 +0300

    [feat] Update

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Config:
+    pass
+
+class ProdConfig:
+    pass
+
+class DevConfig:
+
+    DEBUG = True
+
+# config_options = {
+# 'development':DevConfig,
+# 'production':ProdConfig
+# }
